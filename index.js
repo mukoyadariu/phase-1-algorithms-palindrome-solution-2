a967b451cb0af14977a6d5b03a99ca3f3cc4ab94
mukoyadarius <mukoya.darius@student.moringaschool.com>
2023-07-20 21:46:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -8,7 +8,35 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
-*/
+*/// index.js
+
+// Function to check if a string is a palindrome
+function isPalindrome(str) {
+  // Convert the string to lowercase to make the comparison case-insensitive
+  str = str.toLowerCase();
+
+  // Two pointers to check characters from both ends of the string
+  let left = 0;
+  let right = str.length - 1;
+
+  // Loop until the two pointers meet in the middle
+  while (left < right) {
+    // If characters at the two pointers are not equal, the string is not a palindrome
+    if (str[left] !== str[right]) {
+      return false;
+    }
+
+    // Move the pointers inward
+    left++;
+    right--;
+  }
+
+  // If the loop completes, the string is a palindrome
+  return true;
+}
+
+module.exports = isPalindrome;
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
